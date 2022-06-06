fe56cd0125d7a822e82be0f5a2a81773d980c6a4
Ayushi <t-ayarya@expediagroup.com>
2022-06-06 17:44:13 +0530

    first commit

---
+++ src/main/java/com/example/kafkademo/KafkaListeners.java
@@ -0,0 +1,16 @@
+package com.example.kafkademo;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+    @KafkaListener(
+            topics = "topic1",
+            groupId = "groupId"
+
+    )
+    void listener(String data){
+        System.out.println("Listener received" + data);
+    }
+}
---
+++ src/main/java/com/example/kafkademo/config/KafkaTopicConfig.java
@@ -0,0 +1,17 @@
+package com.example.kafkademo.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+//to create topics
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic randomTopic(){
+        return TopicBuilder.name("topic1")
+                .build();
+    }
+}
